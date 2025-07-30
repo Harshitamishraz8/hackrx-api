dceab851464549664212d135df12bd599b254cf2
Harshita Mishra <136710079+Harshitamishraz8@users.noreply.github.com>
2025-07-30 18:16:38 +0530

    Fix NPM Dependencies and Package Versions

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-const multer = require('multer');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
